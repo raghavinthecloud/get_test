9241b82847f89cf77bf864fadb2e134f8f1d8ffc
raghav karkera <karkeraraghav@gmail.com>
2017-08-14 13:35:28 -0400

    GET and POST
    simple app

--- get_test/ViewController.swift
+++ get_test/ViewController.swift
@@ -20,6 +20,48 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func get(_ sender: Any) {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/users") else {return}
+        
+        let session = URLSession.shared
+        session.dataTask(with: url) { (data, responce, error) in
+            if let responce = responce{
+                print(responce)
+            }
+            if let data = data{
+                print(data)
+            }
+            
+            }.resume()
+    }
+    @IBAction func post(_ sender: Any) {
+        let parameters = ["username":"raghav","tweet":"hello world"]
+        
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/users") else {return}
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+       guard let httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: []) else {return}
+        request.httpBody = httpBody
+        
+        let session = URLSession.shared
+        session.dataTask(with: request) { (data, responce, erroe) in
+            if let responce = responce{
+                print(responce)
+            }
+            if let data = data{
+                do{
+                    let json = try JSONSerialization.jsonObject(with: data, options: [])
+                    print(json)
+                }catch{
+                    print(error)
+                }
+            }
+        }.resume()
+        
+        
+        
+    }
+    
 }
 
